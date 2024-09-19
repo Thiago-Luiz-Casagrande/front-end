ece1b39a7cf01e3e667282dd64e7d5698cc18361
Thiago-Luiz-casagrande <thiagocasagrande2004@gmail.com>
2024-09-18 23:37:34 -0300

    b-Aula-12

--- b-Aula-11/eventos.js
+++ b-Aula-11/eventos.js
@@ -1,5 +1,5 @@
 function salvar(texto){
-    window.sessionStorage.setItem('cor', texto)
+    window.localStorage.setItem('cor', texto)
 }
 
 document.getElementById('inputTexto').addEventListener('blur', (event) => {
